b58c7ef5df24b0aa924ea5b075c0d30c90a0bef3
soHyn <sohyn2208@gmail.com>
2024-02-14 14:36:28 +0900

    feat : 프로젝트 생성

---
+++ src/main/java/com/elecgotchi/elecgotchibackend/ElecgotchiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.elecgotchi.elecgotchibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElecgotchiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElecgotchiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elecgotchi/elecgotchibackend/ElecgotchiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elecgotchi.elecgotchibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElecgotchiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
